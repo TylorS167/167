a332be4eca848f1fc6f0121d3a31c9aced229c16
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-03-31 03:17:20 -0400

    feat(curry): export curry functions

--- src/function/index.ts
+++ src/function/index.ts
@@ -6,3 +6,6 @@ export * from './ascend'
 export * from './binary'
 export * from './bind'
 export * from './id'
+
+// reimplement locally
+export { curry, curry1, curry2, curry3, curry4, curry5 } from '@typed/curry'
