4d75e8dada51bf62b3bda28a8ef6553911a8a145
Brian Cavalier <brian@briancavalier.com>
2017-05-25 16:27:20 -0400

    feat(onlyWhen): add onlyWhen

---
+++ src/function/onlyWhen/onlyWhen.test.ts
@@ -0,0 +1,20 @@
+import * as assert from 'assert'
+
+import { id } from '..'
+import { onlyWhen } from './onlyWhen'
+
+describe(`onlyWhen`, () => {
+  describe(`given a predicate that is false`, () => {
+    it(`returns default value`, () => {
+      const expected = 0
+      assert.strictEqual(expected, onlyWhen((_: number) => false, expected, id, 1))
+    })
+  })
+
+  describe(`given a predicate that is false`, () => {
+    it(`returns default value`, () => {
+      const expected = 1
+      assert.strictEqual(2, onlyWhen((x: number) => x === expected, 0, (x: number) => x + 1, expected))
+    })
+  })
+})
