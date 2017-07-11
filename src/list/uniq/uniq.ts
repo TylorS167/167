530fb5d3ade9384abcded03075431be900634cc3
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-11 13:42:04 -0400

    fix(list): fix uniq type signature
    closes #5

--- src/list/uniq/uniq.ts
+++ src/list/uniq/uniq.ts
@@ -1,1 +1,7 @@
-export const uniq = <A>(list: Array<A>): ReadonlyArray<A> => Array.from(new Set<A>(list))
+export const uniq: Uniq = <A>(list: Array<A>): ReadonlyArray<A> => Array.from(new Set<A>(list))
+
+export interface Uniq {
+  <A>(list: Array<A>): ReadonlyArray<A>
+  <A>(list: ArrayLike<A>): ReadonlyArray<A>
+  <A>(list: ReadonlyArray<A>): ReadonlyArray<A>
+}
