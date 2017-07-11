09eb2c37bb0c569ca380c26e69f7dbd8bdbdf2ea
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-07-11 13:59:35 -0400

    fix(lenses): fix updateAt type signature

--- src/lenses/updateAt/updateAt.ts
+++ src/lenses/updateAt/updateAt.ts
@@ -12,14 +12,14 @@ export const updateAt: UpdateAtArity3 = curry3(
 )
 
 export interface UpdateAtArity3 {
-  <A, B>(lens: Lens<A, B>, f: (value: B) => B, obj: A): A
-  <A, B>(lens: Lens<A, B>, f: (value: B) => B): UpdateAtArity1<A>
+  <A, B>(lens: Lens<A, B>, f: (value: B | void) => B, obj: A): A
+  <A, B>(lens: Lens<A, B>, f: (value: B | void) => B): UpdateAtArity1<A>
   <A, B>(lens: Lens<A, B>): UpdateAtArity2<A, B>
 }
 
 export interface UpdateAtArity2<A, B> {
-  (f: (value: B) => B, obj: A): A
-  (f: (value: B) => B): UpdateAtArity1<A>
+  (f: (value: B | void) => B, obj: A): A
+  (f: (value: B | void) => B): UpdateAtArity1<A>
 }
 
 export type UpdateAtArity1<A> = (obj: A) => A
