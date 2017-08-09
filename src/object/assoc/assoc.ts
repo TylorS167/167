7fb5a90da3c02532077b9ae4e7b2449cc26163d7
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-08-09 12:51:00 -0400

    fix(assoc): correct assoc type-signature
    Thank you @acutmore via Twitter!

--- src/object/assoc/assoc.ts
+++ src/object/assoc/assoc.ts
@@ -1,13 +1,4 @@
 import { AssocArity3 } from './types'
-import { clone } from '../clone'
-import { curry3 } from '../../function/curry/curry3'
+import { set } from '../set'
 
-export const assoc: AssocArity3 = curry3(
-  function assoc<A extends object, B>(path: string, value: B, obj: A): A & { readonly [S in typeof path]: B } {
-    const newObj = clone(obj) as A & { [S in typeof path]: B }
-
-    newObj[path] = value
-
-    return newObj
-  },
-)
+export const assoc: AssocArity3 = set
--- src/object/assoc/types.ts
+++ src/object/assoc/types.ts
@@ -1,14 +1,14 @@
 export interface AssocArity3 {
-  <A extends object, B>(path: string, value: B, obj: A): Readonly<A> & { readonly [S in typeof path]: B }
-  <A extends object, B>(path: string, value: B): AssocArity1<A, typeof path, B>
-  <A extends object>(path: string): AssocArity2<A, typeof path>
+  <K extends string, A extends object, B>(path: K, value: B, obj: A): Readonly<A> & { readonly [S in K]: B }
+  <K extends string, A extends object, B>(path: K, value: B): AssocArity1<A, K, B>
+  <K extends string, A extends object>(path: K): AssocArity2<A, K>
 }
 
-export interface AssocArity2<A extends object, P extends string> {
-  <B>(value: B, obj: A): Readonly<A> & { readonly [S in P]: B }
-  <B>(value: B): AssocArity1<A, P, B>
+export interface AssocArity2<A extends object, Key extends string> {
+  <B>(value: B, obj: A): Readonly<A> & { readonly [K in Key]: B }
+  <B>(value: B): AssocArity1<A, Key, B>
 }
 
-export interface AssocArity1<A extends object, P extends string, B> {
-  (obj: A): Readonly<A> & { readonly [S in P]: B }
+export interface AssocArity1<A extends object, Key extends string, B> {
+  (obj: A): Readonly<A> & { readonly [K in Key]: B }
 }
