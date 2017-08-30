208790b36723ffc38251e4e9e4d5772084ac3644
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-08-30 19:06:26 -0400

    fix(and): accept boolean type values

--- src/logic/and/types.ts
+++ src/logic/and/types.ts
@@ -4,16 +4,31 @@ export interface And {
   (a: false, b: true): false;
   (a: true, b: false): false;
 
+  (a: boolean, b: true): boolean
+  (a: boolean, b: false): false;
+  (a: true, b: boolean): boolean
+  (a: false, b: boolean): false;
+  (a: boolean, b: boolean): boolean
+
   (a: true): AndT
   (a: false): AndF
+  (a: boolean): AndBool
 }
 
 export interface AndT {
   (b: true): true;
-  (b: false): false
+  (b: false): false;
+  (b: boolean): boolean
 }
 
 export interface AndF {
   (b: true): false;
-  (b: false): false
+  (b: false): false;
+  (b: boolean): false
+}
+
+export interface AndBool {
+  (b: true): boolean
+  (b: false): false;
+  (b: boolean): boolean
 }
